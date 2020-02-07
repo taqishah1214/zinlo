a843dd6aaca2b27680b4db2c7401252a30167e59
hammadkhan <hammad.khan@gems.techverx.com>
2020-02-07 20:19:54 +0500

    Import missing namespace

--- aspnet-core/src/Zinlo.Application/Attachments/AttachmentsAppService.cs
+++ aspnet-core/src/Zinlo.Application/Attachments/AttachmentsAppService.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
