cdaeaae49d983bf6a53ee0956ad6969d5ce5e5ef
IhsanTechverx <59415767+IhsanTechverx@users.noreply.github.com>
2020-03-05 12:06:20 +0500

    Amortization and itemization api's and integration of create items.

--- angular/src/app/main/reconcilliation/itemized/create-edit-itemized/create-edit-itemized.component.ts
+++ angular/src/app/main/reconcilliation/itemized/create-edit-itemized/create-edit-itemized.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild, Injector } from '@angular/core';
-import { AccountSubTypeServiceProxy, ChartsofAccountServiceProxy, CreateOrEditChartsofAccountDto, CreateOrEditItemizationDto, ItemizationServiceProxy } from '@shared/service-proxies/service-proxies';
+import { ChartsofAccountServiceProxy, CreateOrEditItemizationDto, ItemizationServiceProxy } from '@shared/service-proxies/service-proxies';
 import { Router } from '@angular/router';
 import { UserListComponentComponent } from '@app/main/checklist/user-list-component/user-list-component.component';
 import { AppComponentBase } from '@shared/common/app-component-base';
@@ -12,131 +12,19 @@ import { AppConsts } from '@shared/AppConsts';
   styleUrls: ['./create-edit-itemized.component.css']
 })
 export class CreateEditItemizedComponent extends AppComponentBase implements OnInit  {
-
-  accountSubTypeName: any;
-  accountSubTypeList: any;
-  accountType: any;
-  accountId : number;
-  selectedUserID : any;
-  editAccountCheck :  boolean = false;
-  accountTypeList: Array<{ id: number, name: string }> = [{ id: 1, name: "Fixed" }, { id: 2, name: "Assets" }, { id: 3, name: "Liability" }];
-  reconcillationTypeList: Array<{ id: number, name: string }> = [{ id: 1, name: "Itemized" }, { id: 2, name: "Amortization" }]
-  reconcillationType: any;
-  assigniId : number;
-  accountDto: CreateOrEditChartsofAccountDto = new CreateOrEditChartsofAccountDto();
   itemizedDto : CreateOrEditItemizationDto = new CreateOrEditItemizationDto();
-
+  minDate: Date = new Date();
   @ViewChild(UserListComponentComponent, { static: false }) selectedUserId: UserListComponentComponent;
-  constructor(private _accountSubTypeService: AccountSubTypeServiceProxy,
-              private _itemizationServiceProxy : ItemizationServiceProxy,
+  constructor(private _itemizationServiceProxy : ItemizationServiceProxy,
     private _chartOfAccountService: ChartsofAccountServiceProxy, private _router: Router, injector: Injector) {
     super(injector)
   }
 
   ngOnInit() {   
-   this.accountId = history.state.data.id;
-    this.getAccountSubTypeForDropDown();
-    if ( this.accountId != 0)
-    {
-      
-      this.editAccount()
-    }
-    else
-    {    
-      this.createNewAccount();
-    }   
+  
   }
-
-  createNewAccount() : void {
-    this.reconcillationType = "Select Reconcillation Type"
-    this.accountType = "Select Account Type"
-    this.accountSubTypeName = "Select Account Sub Type"
-    this.selectedUserID = 0;
-  }
-
-
-  getAccountSubTypeForDropDown(): void {
-    this._accountSubTypeService.accountSubTypeDropDown().subscribe(result => {
-      this.accountSubTypeList = result;
-    });
-  }
-
-  editAccount() : void {
-    this.editAccountCheck = true;
-    this._chartOfAccountService.getAccountForEdit( this.accountId).subscribe(result => {
-       this.accountDto.accountName = result.accountName
-       this.accountDto.accountNumber = result.accountNumber
-       this.accountType = this.getNameofAccountTypeAndReconcillation(result.accountType,"accountType")
-       this.reconcillationType = this.getNameofAccountTypeAndReconcillation(result.reconcillationType,"reconcillation")
-       this.accountSubTypeName =result.accountSubType
-       this.accountDto.accountSubTypeId = result.accountSubTypeId;
-       this.accountDto.id = result.id;
-       this.accountDto.accountType = result.accountType
-       this.selectedUserID = result.assigniId;
-       this.accountDto.reconciliationType = result.reconcillationType
-       this.accountDto.assigneeId = result.assigniId
-    })
-  }
-
-  getNameofAccountTypeAndReconcillation(id , key ) : string {  
-    var result = "" ;
-    if (key === "accountType")
-     {
-      this.accountTypeList.forEach(i => {
-        if  (i.id == id)
-        {
-          result = i.name
-        }
-      })
-      return result;
-     }
-     else if (key === "reconcillation")
-     {
-      this.reconcillationTypeList.forEach(i => {
-        if  (i.id == id)
-        {
-          result = i.name
-        }
-      })
-      return result;
-     }  
-  }
-
- 
-
-
-
-  onSubmit(): void {
-    if (this.editAccountCheck) {
-      this.updateAccount()
-    }
-    else {
-      this.createAccount()
-    }
-  }
-
-  createAccount():void {
-    this.accountDto.assigneeId = Number(this.selectedUserId.selectedUserId);
-    this._chartOfAccountService.createOrEdit(this.accountDto).subscribe(response => {
-      this.notify.success(this.l('Account Successfully Created.'));
-      this.redirectToAccountsList();
-    }) 
-  }
-
- 
-  updateAccount() : void {   
-    if (this.selectedUserId.selectedUserId != undefined)
-    {
-      this.accountDto.assigneeId = Number(this.selectedUserId.selectedUserId);
-    }
-    this._chartOfAccountService.createOrEdit(this.accountDto).subscribe(response => {
-      this.notify.success(this.l('Account Successfully Updated.'));
-      this.redirectToAccountsList();
-    })
-  }
-
-  redirectToAccountsList () : void {
-    this._router.navigate(['/app/main/account/accounts']);
+  redirectToItemsList () : void {
+    this._router.navigate(['/app/main/reconcilliation/itemized']);
   }
   settings: UppyConfig = {
     uploadAPI: {
@@ -153,5 +41,10 @@ export class CreateEditItemizedComponent extends AppComponentBase implements OnI
     };
     container.setViewMode('month');
   }
-
+  SaveChanges(){
+    this._itemizationServiceProxy.createOrEdit(this.itemizedDto).subscribe(response => {
+      this.notify.success(this.l('Item Successfully Created.'));
+      this.redirectToItemsList();
+    }) 
+  }
 }
--- angular/src/app/main/reconcilliation/itemized/itemized.component.ts
+++ angular/src/app/main/reconcilliation/itemized/itemized.component.ts
@@ -1,24 +1,28 @@
-import { Component, Injector } from '@angular/core';
+import { Component, Injector, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { AppComponentBase } from '@shared/common/app-component-base';
 import * as _ from 'lodash';
-import { CategoriesServiceProxy } from '@shared/service-proxies/service-proxies';
+import { CategoriesServiceProxy, ItemizationServiceProxy } from '@shared/service-proxies/service-proxies';
 import { UserInformation } from '../../CommonFunctions/UserInformation';
-import { LazyLoadEvent } from 'primeng/api';
 import { Paginator } from 'primeng/paginator';
 import { Table } from 'primeng/table';
+import { LazyLoadEvent } from 'primeng/api';
 @Component({
   selector: 'app-itemized',
   templateUrl: './itemized.component.html',
   styleUrls: ['./itemized.component.css']
 })
 export class ItemizedComponent extends AppComponentBase {
+  filterText:any;
+  items:any;
 UserProfilePicture: any;
 monthValue: Date = new Date();
 commantBox: boolean;
+@ViewChild('dataTable', { static: true }) dataTable: Table;
+@ViewChild('paginator', { static: true }) paginator: Paginator;
 constructor(
 injector: Injector,
-private _categoriesServiceProxy: CategoriesServiceProxy,
+private _itemizationServiceProxy : ItemizationServiceProxy,
 private _router: Router,
 private userInfo: UserInformation
 
@@ -30,6 +34,27 @@ this.commantBox = true;
 this.getProfilePicture();
 
 }
+
+getAllItems(event?: LazyLoadEvent) {
+  if (this.primengTableHelper.shouldResetPaging(event)) {
+      this.paginator.changePage(0);
+      return;
+  }
+  this.primengTableHelper.showLoadingIndicator();
+  this._itemizationServiceProxy.getAll(
+      this.filterText,
+      this.primengTableHelper.getSorting(this.dataTable),
+      this.primengTableHelper.getSkipCount(this.paginator, event),
+      this.primengTableHelper.getMaxResultCount(this.paginator, event)
+  ).subscribe(result => {
+      this.primengTableHelper.totalRecordsCount = result.totalCount;
+      this.primengTableHelper.records = result.items;
+      this.primengTableHelper.hideLoadingIndicator();
+      this.items = result.items;
+      console.log(this.items);
+  });
+}
+
 
 onOpenCalendar(container) {
 container.monthSelectHandler = (event: any): void => {
@@ -47,15 +72,22 @@ if (this.UserProfilePicture == undefined) {
 this.UserProfilePicture = "";
 }
 }
+
+
 commentClick(): void {
-this.commantBox = false;
+  this.commantBox = false;
+  }
+  
+  onComment(): void {
+  this.commantBox = true;
+  }
+  onCancelComment(): void {
+  this.commantBox = true;
+  }
+  RedirectToAddNew(){
+    this._router.navigate(['/app/main/reconcilliation/itemized/create-edit-itemized']);
+  }
+  
+
 }
 
-onComment(): void {
-this.commantBox = true;
-}
-onCancelComment(): void {
-this.commantBox = true;
-}
-
-}
