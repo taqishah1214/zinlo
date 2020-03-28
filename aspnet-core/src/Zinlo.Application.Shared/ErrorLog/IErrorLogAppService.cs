516b57a347fbc797b4d093dc8666b0a2723805ba
Taqi-Techverx <37609524+Taqi-Techverx@users.noreply.github.com>
2020-03-28 17:40:23 +0500

    Update IErrorLogAppService

--- aspnet-core/src/Zinlo.Application.Shared/ErrorLog/IErrorLogAppService.cs
+++ aspnet-core/src/Zinlo.Application.Shared/ErrorLog/IErrorLogAppService.cs
@@ -1,10 +1,11 @@
 ﻿using System.Threading.Tasks;
+using Abp.Application.Services;
 using Abp.Application.Services.Dto;
 using Zinlo.ErrorLog.Dto;
 
 namespace Zinlo.ErrorLog
 {
-   public interface IErrorLogAppService
+   public interface IErrorLogAppService : IApplicationService
     {
         Task<PagedResultDto<ErrorLogForViewDto>> GetAll(GetAllErroLogInput input);
     }
