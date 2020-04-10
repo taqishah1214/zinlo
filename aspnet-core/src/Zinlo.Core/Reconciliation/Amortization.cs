19e68b39825d983c3abb702c857dc08c088ce110
hammadkhan <hammad.khan@gems.techverx.com>
2020-04-10 19:50:30 +0500

    Audited in itemzied na amortized

--- aspnet-core/src/Zinlo.Core/Reconciliation/Amortization.cs
+++ aspnet-core/src/Zinlo.Core/Reconciliation/Amortization.cs
@@ -1,4 +1,5 @@
-﻿using Abp.Domain.Entities;
+﻿using Abp.Auditing;
+using Abp.Domain.Entities;
 using Abp.Domain.Entities.Auditing;
 using System;
 using System.Collections.Generic;
@@ -6,8 +7,10 @@ using System.Text;
 
 namespace Zinlo.Reconciliation
 {
+    [Audited]
     public class Amortization : CreationAuditedEntity<long>, IMustHaveTenant
     {
+        [DisableAuditing]
         public int TenantId { get; set; }
         public string InoviceNo { get; set; }
         public string JournalEntryNo { get; set; }
@@ -16,9 +19,12 @@ namespace Zinlo.Reconciliation
         public double Amount { get; set; }
         public double AccomulateAmount { get; set; }
         public string Description { get; set; }
+        [DisableAuditing]
         public virtual ChartofAccounts.ChartofAccounts ChartsofAccount { get; set; }
+        [DisableAuditing]
         public long ChartsofAccountId { get; set; }
         public Criteria Criteria { get; set; }
+        [DisableAuditing]
         public DateTime ClosingMonth { get; set; }
 
 
