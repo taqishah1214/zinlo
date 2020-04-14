49d7f2559035334cacbfeee16354e33222cff0a7
MoeenAhmad12 <moeen.ahmad@techverx.com>
2020-04-14 16:33:04 +0500

    Subscription Plans added

--- angular/src/account/register/register.component.ts
+++ angular/src/account/register/register.component.ts
@@ -7,14 +7,36 @@ import { SessionServiceProxy, UpdateUserSignInTokenOutput } from '@shared/servic
 import { UrlHelper } from 'shared/helpers/UrlHelper';
 import { RecaptchaComponent } from 'ng-recaptcha';
 import { AppConsts } from '@shared/AppConsts';
+import { ViewEncapsulation } from '@angular/core';
 
+import {
+    EditionSelectDto,
+    EditionWithFeaturesDto,
+    EditionsSelectOutput,
+    FlatFeatureSelectDto,
+    SubscriptionServiceProxy,
+    TenantRegistrationServiceProxy,
+    EditionPaymentType,
+    SubscriptionStartType
+} from '@shared/service-proxies/service-proxies';
+import * as _ from 'lodash';
 
 declare var $: any;
 @Component({
     templateUrl: './register.component.html',
+    styleUrls: ['./select-edition.component.less'],
+    encapsulation: ViewEncapsulation.None,
     animations: [accountModuleAnimation()]
 })
 export class RegisterComponent extends AppComponentBase implements OnInit {
+    editionsSelectOutput: EditionsSelectOutput = new EditionsSelectOutput();
+    isUserLoggedIn = false;
+    isSetted = false;
+    editionPaymentType: typeof EditionPaymentType = EditionPaymentType;
+    subscriptionStartType: typeof SubscriptionStartType = SubscriptionStartType;
+    /*you can change your edition icons order within editionIcons variable */
+    editionIcons: string[] = ['flaticon-open-box', 'flaticon-rocket', 'flaticon-gift', 'flaticon-confetti', 'flaticon-cogwheel-2', 'flaticon-app', 'flaticon-coins', 'flaticon-piggy-bank', 'flaticon-bag', 'flaticon-lifebuoy', 'flaticon-technology-1', 'flaticon-cogwheel-1', 'flaticon-infinity', 'flaticon-interface-5', 'flaticon-squares-3', 'flaticon-interface-6', 'flaticon-mark', 'flaticon-business', 'flaticon-interface-7', 'flaticon-list-2', 'flaticon-bell', 'flaticon-technology', 'flaticon-squares-2', 'flaticon-notes', 'flaticon-profile', 'flaticon-layers', 'flaticon-interface-4', 'flaticon-signs', 'flaticon-menu-1', 'flaticon-symbol'];
+
     firstName:string;
     lastName:string;
     email:string;
@@ -35,11 +57,55 @@ export class RegisterComponent extends AppComponentBase implements OnInit {
     commitment:string;
     constructor(
         injector: Injector,
+        private _tenantRegistrationService: TenantRegistrationServiceProxy,
+        private _subscriptionService: SubscriptionServiceProxy,
+        private _router: Router
     ) {
         super(injector);
     }
 
-    ngOnInit() {
+    ngOnInit() {this.isUserLoggedIn = abp.session.userId > 0;
+
+        this._tenantRegistrationService.getEditionsForSelect()
+            .subscribe((result) => {
+                this.editionsSelectOutput = result;
+
+                if (!this.editionsSelectOutput.editionsWithFeatures || this.editionsSelectOutput.editionsWithFeatures.length <= 0) {
+                    this._router.navigate(['/account/register-tenant']);
+                }
+            });
+    }
+    
+    isFree(edition: EditionSelectDto): boolean {
+        return !edition.monthlyPrice && !edition.annualPrice;
+    }
+
+    isTrueFalseFeature(feature: FlatFeatureSelectDto): boolean {
+        return feature.inputType.name === 'CHECKBOX';
+    }
+
+    featureEnabledForEdition(feature: FlatFeatureSelectDto, edition: EditionWithFeaturesDto): boolean {
+        const featureValues = _.filter(edition.featureValues, { name: feature.name });
+        if (!featureValues || featureValues.length <= 0) {
+            return false;
+        }
+
+        const featureValue = featureValues[0];
+        return featureValue.value.toLowerCase() === 'true';
+    }
+
+    getFeatureValueForEdition(feature: FlatFeatureSelectDto, edition: EditionWithFeaturesDto): string {
+        const featureValues = _.filter(edition.featureValues, { name: feature.name });
+        if (!featureValues || featureValues.length <= 0) {
+            return '';
+        }
+
+        const featureValue = featureValues[0];
+        return featureValue.value;
+    }
+
+    upgrade(upgradeEdition: EditionSelectDto, editionPaymentType: EditionPaymentType): void {
+        this._router.navigate(['/account/upgrade'], { queryParams: { upgradeEditionId: upgradeEdition.id, editionPaymentType: editionPaymentType } });
     }
 
     current: number = 0;
