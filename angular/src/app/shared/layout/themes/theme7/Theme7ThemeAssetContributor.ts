4e162fe3b366daf6855f0beed49b88b5c7af0c7c
Taqi-Techverx <37609524+Taqi-Techverx@users.noreply.github.com>
2020-05-11 10:58:17 +0500

    remove unnecessary themes from projetc

--- angular/src/app/shared/layout/themes/theme7/Theme7ThemeAssetContributor.ts
+++
@@ -1,8 +0,0 @@
-import { IThemeAssetContributor } from '../ThemeAssetContributor';
-import { AppConsts } from '@shared/AppConsts';
-
-export class Theme7ThemeAssetContributor implements IThemeAssetContributor {
-    getAssetUrls(): string[] {
-        return [AppConsts.appBaseUrl + '/assets/fonts/fonts-montserrat.min.css'];
-    }
-}
