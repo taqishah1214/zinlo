a8796bcde72efd704708b808590a1efa45ebc2b3
MoeenAhmad12 <moeen.ahmad@techverx.com>
2020-05-15 16:04:53 +0500

    Subscription component added

---
+++ angular/src/app/admin/subscription-management/subscription.component.ts
@@ -0,0 +1,10 @@
+import { Component, Injector, OnInit, ViewChild } from '@angular/core';
+
+
+@Component({
+    templateUrl: './subscription.component.html',
+    styleUrls: ['./subscription.component.css'],
+})
+
+export class SubscriptionComponent  {
+}
